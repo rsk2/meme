4997a101737ca0056cdd3c96979ca1edda83a0c6
rohan <rsureshkumar@empyra.com>
2019-05-12 19:14:12 +0530

    Updates

---
+++ precache-manifest.5dd27438677557797cb31b50b3c7e1fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meme/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "41968b294926a2d87494",
+    "url": "/meme/static/js/runtime~main.1c7c63e9.js"
+  },
+  {
+    "revision": "02f5f43685d5c536477f",
+    "url": "/meme/static/js/main.ffe47255.chunk.js"
+  },
+  {
+    "revision": "d4b87d5598e6173ca779",
+    "url": "/meme/static/js/2.d0fe9ba5.chunk.js"
+  },
+  {
+    "revision": "02f5f43685d5c536477f",
+    "url": "/meme/static/css/main.456e81a5.chunk.css"
+  },
+  {
+    "revision": "6caa453a8757ed21af0edf3836442b73",
+    "url": "/meme/index.html"
+  }
+];
